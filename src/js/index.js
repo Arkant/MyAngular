f84f77d0982bfee493a04202d5e05f71b6e67e1c
Rostyslav Yashkin <pan_rostislav@ukr.net>
2019-02-12 13:33:47 +0200

    add compile method

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-console */
 (function() {
-  const directives = [];
+  const directives = [{
+    name: 'ng-click',
+    functions: [function(element, cb) {
+      element.addEventListener('click', function() {
+        const fn = new Function(cb);
+        fn();
+      });
+    }]
+  }];
 
   const smallAngular = {
     directive(ngName, cb) {
@@ -11,21 +19,25 @@
     },
     compile(node) {
       for (let i = 0; i <= node.length - 1; i++) {
-        console.log(`${node[i].name} -> ${node[i].value}`);
+        directives.forEach(element => {
+          if (node[i].name === element.name) {
+            element.method();
+          }
+        });
       }
     },
-    bootstrap(node) {
-      const appWrapper = document.querySelector('[ng-app]');
+    bootstrap(node = '[ng-app]') {
+      const appWrapper = document.querySelector(node);
       const child = appWrapper.children;
-      console.log(child);
 
-      [...child].map(item =>
+      [...child].forEach(item =>
         this.compile(item.attributes)
       );
     }
   };
 
   window.smallAngular = smallAngular;
-  smallAngular.directive('ng-click', () => console.log('i am ng-click'));
+  // smallAngular.directive('ng-click', () => console.log('i am ng-click'));
+  // smallAngular.directive('ng-init', () => console.log('i am ng-init'));
   smallAngular.bootstrap();
 }());
